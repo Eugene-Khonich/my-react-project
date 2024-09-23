140a0dfcc029315fddb823c2a0ea9ac0433468c2
Eugene-Khonich <ehonich@gmail.com>
2024-09-24 00:24:29 +0300

    1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+// import { Alert } from '../Alert/Alert';
+// import { Product } from '../Product/Product';
+
+// export const App = () => {
+//   return (
+//     <>
+//       <Alert variant="info">
+//         Would you like to browse our recommended products?
+//       </Alert>
+//       <Alert variant="error">
+//         There was an error during your last transaction
+//       </Alert>
+//       <Alert variant="success">
+//         Payment received, thank you for your purchase
+//       </Alert>
+//       <Alert variant="warning">
+//         Please update your profile contact information
+//       </Alert>
+//     </>
+//   );
+// };
+
+const App = () => {
+  return <button>Click me!</button>;
+};
+
+export default App;
