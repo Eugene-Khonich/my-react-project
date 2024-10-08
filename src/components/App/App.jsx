0276d4db2e993e82dbad1b1d663fc63ab1ce65a6
Eugene-Khonich <ehonich@gmail.com>
2024-10-08 21:19:36 +0300

    123

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -51,28 +51,53 @@
 // import LoginForm from '../LoginForm/LoginForm';
 import { useState } from 'react';
 // import { useId } from 'react';
-
-const App = () => {
-  const [hasAccepted, setHasAccepted] = useState(false);
+const LoginForm = () => {
+  const [values, setValues] = useState({
+    login: '',
+    password: '',
+  });
 
   const handleChange = evt => {
-    setHasAccepted(evt.target.checked);
+    setValues({
+      ...values,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  const handleSumit = evt => {
+    evt.preventDefault();
+
+    console.log(values);
+
+    setValues({
+      login: '',
+      password: '',
+    });
   };
 
   return (
+    <form onSubmit={handleSumit}>
+      <input
+        type="text"
+        name="login"
+        value={values.login}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+const App = () => {
+  return (
     <div>
-      <label>
-        <input
-          type="checkbox"
-          name="terms"
-          checked={hasAccepted}
-          onChange={handleChange}
-        />
-        I accept terms and conditions
-      </label>
-      <button type="button" disabled={!hasAccepted}>
-        Proceed
-      </button>
+      <LoginForm />
     </div>
   );
 };
